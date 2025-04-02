c2a3aa70ab50ddb5228fa3eb65085ca1cf0601c1
Sri Hema Saranya Sekar <saranya.s.umashankar@gmail.com>
2025-04-01 20:13:40 -0700

    deleted the LoginPF,modified login

--- src/test/java/pages/LoginpagePF.java
+++ src/test/java/pages/LoginpagePF.java
@@ -7,55 +7,64 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import base.BaseTest;
-  
-public class LoginpagePF{
- 
+
+public class LoginpagePF {
+
 	private WebDriver driver;
 
-public LoginpagePF(WebDriver driver) {
-    this.driver = driver;
-}
-    // Locators
+	public LoginpagePF(WebDriver driver) {
+		this.driver = driver;
+	}
+	// Locators
 
-private By getStartedBtn = By.xpath("//div[@class='content']/a/button");
-private By signInLink = By.xpath("//div[@class='navbar-nav'][2]/ul/a[3]");
-private By usernameField = By.id("id_username");
-private By passwordField = By.id("id_password");
-private By loginSubmitBtn = By.xpath("//div[@class='col-sm']/form/input[4]");
-private By successMessage = By.xpath("//div[@class='alert alert-success']");
-private By errorMessage = By.xpath("//div[@role='alert']");
-  
+	private By getStartedBtn = By.xpath("//div[@class='content']/a/button");
+	private By signInLink = By.xpath("//div[@class='navbar-nav'][2]/ul/a[3]");
+	private By usernameField = By.id("id_username");
+	private By passwordField = By.id("id_password");
+	private By loginSubmitBtn = By.xpath("//div[@class='col-sm']/form/input[4]");
+	private By successMessage = By.xpath("//div[@class='alert alert-success']");
+	private By errorMessage = By.xpath("//div[@role='alert']");
+
+	public void userCredentials() {
+		driver.findElement(getStartedBtn).click();
+		driver.findElement(signInLink).click();
+		driver.findElement(usernameField).sendKeys("SeleniumSquad");
+		driver.findElement(passwordField).sendKeys("Squad2025#");
+		driver.findElement(loginSubmitBtn).click();
+
+	}
+
 //Actions for the Login Page
-public void clickGetStarted() {
- driver.findElement(getStartedBtn).click();
-}
+	public void clickGetStarted() {
+		driver.findElement(getStartedBtn).click();
+	}
 
-public void clickSignIn() {
- driver.findElement(signInLink).click();
-}
+	public void clickSignIn() {
+		driver.findElement(signInLink).click();
+	}
 
-public void enterUsername(String username) {
- WebElement field = driver.findElement(usernameField);
- field.clear();
- field.sendKeys(username);
-}
+	public void enterUsername(String username) {
+		WebElement field = driver.findElement(usernameField);
+		field.clear();
+		field.sendKeys(username);
+	}
 
-public void enterPassword(String password) {
- WebElement field = driver.findElement(passwordField);
- field.clear();
- field.sendKeys(password);
-}
+	public void enterPassword(String password) {
+		WebElement field = driver.findElement(passwordField);
+		field.clear();
+		field.sendKeys(password);
+	}
 
-public void submitLogin() {
- driver.findElement(loginSubmitBtn).click();
-}
+	public void submitLogin() {
+		driver.findElement(loginSubmitBtn).click();
+	}
 
-public String getSuccessMessage() {
- return driver.findElement(successMessage).getText();
-}
+	public String getSuccessMessage() {
+		return driver.findElement(successMessage).getText();
+	}
 
-public String getErrorMessage() {
- return driver.findElement(errorMessage).getText();
-}
+	public String getErrorMessage() {
+		return driver.findElement(errorMessage).getText();
+	}
 
 }
