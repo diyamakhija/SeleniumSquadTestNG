7c77b6cb91872d9ade34359d82537cb8cf50b7d4
saranya-sekar-eng <saranya.s.umashankar@gmail.com>
2025-04-01 19:47:06 -0700

    Update LoginPF.java

--- src/test/java/pages/LoginPF.java
+++ src/test/java/pages/LoginPF.java
@@ -3,7 +3,7 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class LoginPF {
+public class LoginPF extends BaseClass{
 
 	@FindBy(xpath = "//div[@class='content']/a/button")
 	public WebElement getStartedBtn;
